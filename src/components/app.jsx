37c6a66c1b9b5a4e51c088b34327459a65b0d315
fujisii <51264835+fujisii@users.noreply.github.com>
2021-01-30 04:00:12 +0900

    done #17

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
+import Greeting from './greeting';
+
 class App extends Component {
-    render() {
-        return (<div>Hello from class App</div>);
-    }
+  render() {
+    return (
+      <div>
+        <Greeting name="Bob" />
+        <Greeting name="John" />
+      </div>
+    );
+  }
 }
 
 export default App;
