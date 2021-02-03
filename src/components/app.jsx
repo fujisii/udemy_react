d833c5a61523a9e6e2f24a02eb1a94db176dd35f
fujisii <51264835+fujisii@users.noreply.github.com>
2021-02-03 22:42:29 +0900

    done #24

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import SearchForm from './SearchForm';
 import GeocodeResult from './GeocodeResult';
+
+const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -9,8 +13,58 @@ class App extends Component {
     };
   }
 
+  setErrorMessage(message) {
+    this.setState({
+      address: message,
+      lat: 0,
+      lng: 0,
+    });
+  }
+
   handlePlaceSubmit(place) {
-    console.log(place);
+    axios
+      .get(GEOCODE_ENDPOINT, { params: { address: place } })
+      .then((results) => {
+        console.log(results);
+        // const data = results.data;
+        const data = {
+          status: 'OK',
+          // status: 'ZERO_RESULTS',
+          // status: 'REQUEST_DENIED',
+          results: [{
+            formatted_address: '日本、〒105-0011 東京都港区芝公園４丁目２−８',
+            geometry: {
+              location: {
+                lat: 35.6585805,
+                lng: 139.7454329,
+              },
+            },
+          }],
+        };
+
+        const result = data.results[0];
+        switch (data.status) {
+          case 'OK': {
+            const location = result.geometry.location;
+            this.setState({
+              address: result.formatted_address,
+              lat: location.lat,
+              lng: location.lng,
+            });
+            break;
+          }
+          case 'ZERO_RESULTS': {
+            this.setErrorMessage('結果が見つかりませんでした');
+            break;
+          }
+          default: {
+            this.setErrorMessage('エラーが発生しました');
+          }
+        }
+      })
+      .catch(() => {
+        this.setErrorMessage('通信に失敗しました');
+      });
   }
 
   render() {
